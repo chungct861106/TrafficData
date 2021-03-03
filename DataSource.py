6e36fe5c1f36eda3f5d0385739c79a19f147ddc1
SolabComputer <thomson861106@gmail.com>
2021-03-03 16:42:12 +0800

    Data Update

--- DataSource.py
+++ DataSource.py
@@ -1,7 +1,16 @@
 import requests
-from requests.auth import HTTPBasicAuth
-url = "https://traffic.transportdata.tw/MOTC/v2/Road/Traffic/SectionShape/Highway?$top=30&$format=JSON"
-params = { '$top':30,'$format':'JSON'}
+import pandas as pd
+
+City = 'Taipei'
+url = "https://traffic.transportdata.tw/MOTC/v2/Road/Traffic/SectionShape/City/{}/".format(City)
+params = { '$count':"true" ,'$format':'JSON'}
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-res = requests.get(url, headers=headers).json()
-print(res.status_code)
+GeoRoad = pd.DataFrame(requests.get(url, params=params, headers=headers).json()['SectionShapes'])
+url = "https://traffic.transportdata.tw/MOTC/v2/Road/Traffic/Section/City/{}/".format(City)
+RoadInfo = pd.DataFrame(requests.get(url, params=params, headers=headers).json()["Sections"])
+RoadInfo.set_index("SectionID", drop=True, inplace=True)
+RoadInfo = RoadInfo.drop(["SectionStart", "SectionEnd"], axis=1).sort_index()
+RoadInfo["Geometry"] = GeoRoad.sort_values("SectionID")["Geometry"].tolist()
+PointsName = RoadInfo["RoadSection"].tolist()
+RoadInfo["Start"] = [p["Start"] for p in PointsName]
+RoadInfo["End"] = [p["End"] for p in PointsName]
